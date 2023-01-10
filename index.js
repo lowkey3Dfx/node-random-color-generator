5441c3adc542a8a1083a62b0a18df202f6b94549
lowkey3Dfx <Arif.Kesdiren@gmail.com>
2023-01-10 20:50:50 +0100

    Add final commit
    Deleted stylelint.sonfig.js

--- index.js
+++ index.js
@@ -4,30 +4,19 @@ import randomColor from 'randomcolor';
 const lum = process.argv[3];
 const col = process.argv[2];
 
-const color = randomColor({
+const userColor = randomColor({
   luminosity: lum,
   hue: col,
 });
 function colorAndLog(str) {
-  console.log(chalk.hex(color).bold(str));
+  console.log(chalk.hex(userColor).bold(str));
 }
-
-// LONG VERSION
-// colorAndLog("##############################");
-// colorAndLog("##############################");
-// colorAndLog("##############################");
-// colorAndLog("########              ########");
-// colorAndLog(`########   ${color}    ########`);
-// colorAndLog("########              ########");
-// colorAndLog("##############################");
-// colorAndLog("##############################");
-// colorAndLog("##############################");
 
 colorAndLog(`##############################
 ##############################
 ##############################
 ########              ########
-########   ${color}    ########
+########   ${userColor}    ########
 ########              ########
 ##############################
 ##############################
