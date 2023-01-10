10a05c62b53ffa1af4ca053e6cf5b296048577a3
lowkey3Dfx <Arif.Kesdiren@gmail.com>
2023-01-10 16:42:54 +0100

    Fix most

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('hello');
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const lum = process.argv[3];
+const col = process.argv[2];
+
+const color = randomColor({
+  luminosity: lum,
+  hue: col,
+});
+function colorAndLog(str) {
+  console.log(chalk.hex(color).bold(str));
+}
+
+//LONG VERSION
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("########              ########");
+// colorAndLog(`########   ${color}    ########`);
+// colorAndLog("########              ########");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+// colorAndLog("##############################");
+
+colorAndLog(`##############################
+##############################
+##############################
+########              ########
+########   ${color}    ########
+########              ########
+##############################
+##############################
+##############################`);
